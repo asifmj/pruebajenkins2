a8de8e4654e60a510fe68dd2147b98424123b361
asifmj <49119359+asifmj@users.noreply.github.com>
2023-05-31 10:28:03 +0200

    Add files via upload

---
+++ src/main/java/com/example/asif/asif/AsifApplication.java
@@ -0,0 +1,13 @@
+package com.example.asif.asif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/asif/asif/AsifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asif.asif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
